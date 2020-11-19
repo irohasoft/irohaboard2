6d2948dda5c677cd7e7d2540d0ee07677af68169
Kotaro Miura <miura@irohasoft.jp>
2020-11-19 13:05:34 +0900

    CakePHP4対応

--- templates/Admin/element/paging.php
+++ templates/Admin/element/paging.php
@@ -1,11 +1,14 @@
-<div class="text-center">
-<?php
-echo $this->Paginator->counter(array(
-	'format' => __('合計').' : {:count}'.__('件').'　{:page} / {:pages}'.__('ページ')
-));
-?>
+<div class="paginator">
+	<div class="text-center">
+		<?= $this->Paginator->counter(__('合計').' : {{count}}'.__('件').'　{{page}} / {{pages}}'.__('ページ')) ?></p>
+	</div>
+	<div class="text-center">
+		<ul class="pagination">
+			<?= $this->Paginator->first('<< ') ?>
+			<?= $this->Paginator->prev('< ') ?>
+			<?= $this->Paginator->numbers() ?>
+			<?= $this->Paginator->next(' >') ?>
+			<?= $this->Paginator->last(' >>') ?>
+		</ul>
+	</div>
 </div>
-<div class="text-center">
-	<?php echo $this->Paginator->pagination(array('ul' => 'pagination')); ?>
-</div>
-
