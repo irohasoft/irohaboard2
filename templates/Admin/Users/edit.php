68804d982576bf85d9398fa95ea5cacc37fcc356
Kotaro Miura <miura@irohasoft.jp>
2020-11-19 17:10:21 +0900

    ユーザ管理画面の実装

--- templates/Admin/Users/edit.php
+++ templates/Admin/Users/edit.php
@@ -4,40 +4,39 @@
  * @var \App\Model\Entity\User $user
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $user->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="users form content">
-            <?= $this->Form->create($user) ?>
-            <fieldset>
-                <legend><?= __('Edit User') ?></legend>
-                <?php
-                    echo $this->Form->control('username');
-                    echo $this->Form->control('password');
-                    echo $this->Form->control('name');
-                    echo $this->Form->control('role');
-                    echo $this->Form->control('email');
-                    echo $this->Form->control('comment');
-                    echo $this->Form->control('last_logined', ['empty' => true]);
-                    echo $this->Form->control('started', ['empty' => true]);
-                    echo $this->Form->control('ended', ['empty' => true]);
-                    echo $this->Form->control('deleted', ['empty' => true]);
-                    echo $this->Form->control('courses._ids', ['options' => $courses]);
-                    echo $this->Form->control('groups._ids', ['options' => $groups]);
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
-    </div>
+<?php use Cake\Core\Configure;?>
+<?php $this->Form->setTemplates(Configure::read('bootstrap_form_template'));?>
+<?= $this->element('admin_menu');?>
+<?= $this->Html->css( 'select2.min.css');?>
+<?= $this->Html->script( 'select2.min.js');?>
+<?php $this->Html->scriptStart(['block' => true]); ?>
+	$(function (e) {
+		$('#groups-ids').select2({placeholder:   "<?= __('所属するグループを選択して下さい。(複数選択可)')?>", closeOnSelect: <?= (Configure::read('close_on_select') ? 'true' : 'false'); ?>,});
+		$('#courses-ids').select2({placeholder:   "<?= __('受講するコースを選択して下さい。(複数選択可)')?>", closeOnSelect: <?= (Configure::read('close_on_select') ? 'true' : 'false'); ?>,});
+	});
+<?php $this->Html->scriptEnd(); ?>
+<div class="admin-infos-edit">
+<?= $this->Html->link(__('<< 戻る'), ['action' => 'index'])?>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<?= ($this->request->getParam('action') == 'edit') ? __('編集') :  __('新規お知らせ'); ?>
+		</div>
+		<div class="panel-body">
+			<?= $this->Form->create($user, ['class' => 'form-horizontal']) ?>
+				<?php
+					echo $this->Form->control('username',		['label' => __('ログインID'), 'required' => true]);
+					echo $this->Form->control('password',		['label' => __('新しいパスワード'), 'required' => true]);
+					//echo $this->Form->control('role',			['label' => __('権限'), 'required' => true, 'type' => 'radio', 'options' => Configure::read('user_role')]);
+//					echo '<label class="col col-sm-3 control-label">権限</label><div class="col col-sm-9">';
+					echo $this->Form->control('role',			['label' => __('権限'), 'required' => true, 'options' => Configure::read('user_role'), 'type' => 'radio', 'hiddenField' => false]);
+//					echo '</div>';
+					echo $this->Form->control('name',			['label' => __('氏名'), 'required' => true]);
+					echo $this->Form->control('email',			['label' => __('メールアドレス'), 'required' => false]);
+					echo $this->Form->control('groups._ids',	['options' => $groups, 'label' => __('対象グループ')]);
+					echo $this->Form->control('courses._ids',	['options' => $groups, 'label' => __('受講コース')]);
+				?>
+			<?= $this->Form->button(__('保存'), Configure::read('form_submit_defaults')) ?>
+			<?= $this->Form->end() ?>
+		</div>
+	</div>
 </div>
--- templates/Admin/Users/index.php
+++ templates/Admin/Users/index.php
@@ -1,64 +1,45 @@
+<?php echo $this->element('admin_menu');?>
 <?php
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\User[]|\Cake\Collection\CollectionInterface $users
  */
 ?>
-<div class="users index content">
-    <?= $this->Html->link(__('New User'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Users') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('username') ?></th>
-                    <th><?= $this->Paginator->sort('password') ?></th>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('role') ?></th>
-                    <th><?= $this->Paginator->sort('email') ?></th>
-                    <th><?= $this->Paginator->sort('last_logined') ?></th>
-                    <th><?= $this->Paginator->sort('started') ?></th>
-                    <th><?= $this->Paginator->sort('ended') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
-                    <th><?= $this->Paginator->sort('deleted') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($users as $user): ?>
-                <tr>
-                    <td><?= $this->Number->format($user->id) ?></td>
-                    <td><?= h($user->username) ?></td>
-                    <td><?= h($user->password) ?></td>
-                    <td><?= h($user->name) ?></td>
-                    <td><?= h($user->role) ?></td>
-                    <td><?= h($user->email) ?></td>
-                    <td><?= h($user->last_logined) ?></td>
-                    <td><?= h($user->started) ?></td>
-                    <td><?= h($user->ended) ?></td>
-                    <td><?= h($user->created) ?></td>
-                    <td><?= h($user->modified) ?></td>
-                    <td><?= h($user->deleted) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $user->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $user->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $user->id], ['confirm' => __('Are you sure you want to delete # {0}?', $user->id)]) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-    </div>
+<div class="admin-users-index">
+	<div class="ib-page-title"><?php echo __('ユーザ一覧'); ?></div>
+	<div class="buttons_container">
+		<button type="button" class="btn btn-primary btn-add" onclick="location.href='<?php echo $this->Url->build(['action' => 'add']) ?>'">+ 追加</button>
+	</div>
+	<table cellpadding="0" cellspacing="0">
+		<thead>
+			<tr>
+				<th><?= $this->Paginator->sort('username',		__('ログインID')) ?></th>
+				<th><?= $this->Paginator->sort('name',			__('氏名')) ?></th>
+				<th><?= $this->Paginator->sort('role',			__('権限')) ?></th>
+				<th><?= $this->Paginator->sort('group_title',	__('所属グループ')) ?></th>
+				<th><?= $this->Paginator->sort('course_title',	__('受講コース')) ?></th>
+				<th><?= $this->Paginator->sort('last_logined',	__('最終ログイン日時')) ?></th>
+				<th><?= $this->Paginator->sort('created',		__('作成日時')) ?></th>
+				<th class="actions"><?= __('Actions') ?></th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($users as $user): ?>
+			<tr>
+				<td><?= h($user->username) ?></td>
+				<td><?= h($user->name) ?></td>
+				<td><?= h($user->role) ?></td>
+				<td><?= h($user->group_title) ?></td>
+				<td><?= h($user->course_title) ?></td>
+				<td><?= h($user->last_logined) ?></td>
+				<td><?= h($user->created) ?></td>
+				<td class="ib-col-action">
+					<?= $this->Html->link(__('編集'), ['action' => 'edit', $user->id], ['class' => 'btn btn-success']) ?>
+					<?= $this->Form->postLink(__('削除'), ['action' => 'delete', $user->id], ['confirm' => __('{0} を削除してもよろしいですか?', $user->title), 'class'=>'btn btn-danger']) ?>
+				</td>
+			</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+	<?php echo $this->element('paging');?>
 </div>
