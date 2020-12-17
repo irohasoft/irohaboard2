b59b228519d48cdb31ac694654a6782abb75f028
Kotaro Miura <miura@irohasoft.jp>
2020-12-17 19:37:58 +0900

    不要なメソッドを削除

--- src/Controller/RecordsController.php
+++ src/Controller/RecordsController.php
@@ -27,22 +27,6 @@ class RecordsController extends AppController
     }
 
     /**
-     * View method
-     *
-     * @param string|null $id Record id.
-     * @return \Cake\Http\Response|null|void Renders view
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $record = $this->Records->get($id, [
-            'contain' => ['Courses', 'Users', 'Contents', 'RecordsQuestions'],
-        ]);
-
-        $this->set(compact('record'));
-    }
-
-    /**
      * Add method
      *
      * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
@@ -63,52 +47,5 @@ class RecordsController extends AppController
         $users = $this->Records->Users->find('list', ['limit' => 200]);
         $contents = $this->Records->Contents->find('list', ['limit' => 200]);
         $this->set(compact('record', 'courses', 'users', 'contents'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Record id.
-     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $record = $this->Records->get($id, [
-            'contain' => [],
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $record = $this->Records->patchEntity($record, $this->request->getData());
-            if ($this->Records->save($record)) {
-                $this->Flash->success(__('The record has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The record could not be saved. Please, try again.'));
-        }
-        $courses = $this->Records->Courses->find('list', ['limit' => 200]);
-        $users = $this->Records->Users->find('list', ['limit' => 200]);
-        $contents = $this->Records->Contents->find('list', ['limit' => 200]);
-        $this->set(compact('record', 'courses', 'users', 'contents'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Record id.
-     * @return \Cake\Http\Response|null|void Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $record = $this->Records->get($id);
-        if ($this->Records->delete($record)) {
-            $this->Flash->success(__('The record has been deleted.'));
-        } else {
-            $this->Flash->error(__('The record could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
     }
 }
