edb9b0f3db295f87d8187038c795af2852856310
Kotaro Miura <miura@irohasoft.jp>
2020-12-21 20:31:31 +0900

    グループ受講登録用テーブルクラスを追加

---
+++ src/Model/Table/GroupsCoursesTable.php
@@ -0,0 +1,87 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+use Cake\Datasource\ConnectionManager;
+
+/**
+ * GroupsCourses Model
+ *
+ * @property \App\Model\Table\GroupsTable&\Cake\ORM\Association\BelongsTo $Groups
+ * @property \App\Model\Table\CoursesTable&\Cake\ORM\Association\BelongsTo $Courses
+ *
+ * @method \App\Model\Entity\GroupsCourse newEmptyEntity()
+ * @method \App\Model\Entity\GroupsCourse newEntity(array $data, array $options = [])
+ * @method \App\Model\Entity\GroupsCourse[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\GroupsCourse get($primaryKey, $options = [])
+ * @method \App\Model\Entity\GroupsCourse findOrCreate($search, ?callable $callback = null, $options = [])
+ * @method \App\Model\Entity\GroupsCourse patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\GroupsCourse[] patchEntities(iterable $entities, array $data, array $options = [])
+ * @method \App\Model\Entity\GroupsCourse|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\GroupsCourse saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\GroupsCourse[]|\Cake\Datasource\ResultSetInterface|false saveMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\GroupsCourse[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
+ * @method \App\Model\Entity\GroupsCourse[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\GroupsCourse[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class GroupsCoursesTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->setTable('ib_groups_courses');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Groups', [
+            'foreignKey' => 'group_id',
+            'joinType' => 'INNER',
+        ]);
+        $this->belongsTo('Courses', [
+            'foreignKey' => 'course_id',
+            'joinType' => 'INNER',
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules): RulesChecker
+    {
+        $rules->add($rules->existsIn(['group_id'], 'Groups'), ['errorField' => 'group_id']);
+        $rules->add($rules->existsIn(['course_id'], 'Courses'), ['errorField' => 'course_id']);
+
+        return $rules;
+    }
+}
