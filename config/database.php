645bdc4f3d6f4c114f1015509d519b016502be93
Kotaro Miura <miura@irohasoft.jp>
2021-01-28 12:53:18 +0900

    データベースの設定を外部ファイル化

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * iroha Board Project
+ *
+ * @author		  Kotaro Miura
+ * @copyright	  2015-2021 iroha Soft, Inc. (https://irohasoft.jp)
+ * @link		  https://irohaboard.irohasoft.jp
+ * @license 	  https://www.gnu.org/licenses/gpl-3.0.en.html GPL License
+ */
+
+return [
+	'Datasources' => [
+		'default' => [
+			'host' => 'localhost',
+			'username' => 'root',
+			'password' => '',
+			'database' => 'irohaboard2',
+			'encoding' => 'utf8',
+		],
+	],
+];
