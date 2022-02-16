52f80008e0143e67ec7a0ba7864ce2f24d8ef156
Kotaro Miura <miura@irohasoft.jp>
2022-02-16 17:52:32 +0900

    検索用フィールドのヘルパーを追加

--- src/View/Helper/AppFormHelper.php
+++ src/View/Helper/AppFormHelper.php
@@ -54,4 +54,107 @@ class AppFormHelper extends FormHelper
 		
 		return $this->control($fieldName, $options);
 	}
+
+	/**
+	 * 検索フィールド
+	 * @param string $fieldName フィールド名
+	 * @param array $additional_options 追加オプション
+	 * @return string 出力タグ
+	 */
+	public function searchField($fieldName, $additional_options = [])
+	{
+		// デフォルトオプション
+		$options = [
+			'class'=>'form-control',
+			'required' => false
+		];
+
+		// 追加オプション
+		foreach($additional_options as $key => $value)
+		{
+			$options[$key] = $value;
+		}
+		
+		if(isset($options['label']))
+			$options['label'] .= ' :';
+		
+		return $this->control($fieldName, $options);
+	}
+
+	/**
+	 * 日付指定リストボックスの出力
+	 * @param string $fieldName フィールド名
+	 * @param array $additional_options 追加オプション
+	 * @return string 出力タグ
+	 */
+	public function controlDate($fieldName, $additional_options = [])
+	{
+		// デフォルトオプション
+		$options = [
+			'type' => 'date',
+		];
+		
+		// 追加オプション
+		foreach($additional_options as $key => $value)
+		{
+			$options[$key] = $value;
+		}
+		
+		if(isset($options['label']))
+		{
+			if($options['label'] != '～')
+				$options['label'] .=  ' :';
+		}
+		
+		return $this->control($fieldName, $options);
+	}
+
+	/**
+	 * 検索用日付指定リストボックスの出力
+	 * @param string $fieldName フィールド名
+	 * @param array $additional_options 追加オプション
+	 * @return string 出力タグ
+	 */
+	public function searchDate($fieldName, $additional_options = [])
+	{
+		// デフォルトオプション
+		$options = [
+			'type' => 'date',
+			'class'=>'form-control',
+		];
+		
+		// 追加オプション
+		foreach($additional_options as $key => $value)
+		{
+			$options[$key] = $value;
+		}
+		
+		if(isset($options['label']))
+		{
+			if($options['label'] != '～')
+				$options['label'] .=  ' :';
+		}
+		
+		return $this->control($fieldName, $options);
+	}
+	/**
+	 * 説明用ブロックの出力
+	 * @param string $label ラベル
+	 * @param string $content 内容
+	 * @param bool $is_bold 太字
+	 * @param string $block_class クラス
+	 * @return string 出力タグ
+	 */
+	public static function block($label, $content, $is_bold = false, $block_class = '')
+	{
+		$content = $is_bold ? '<h5>'.$content.'</h5>' : $content;
+		
+		$tag = 
+			'<div class="form-group '.$block_class.'">'.
+			'  <label for="UserRegistNo" class="col col-sm-3 control-label">'.$label.'</label>'.
+			'  <div class="col col-sm-9">'.$content.'</div>'.
+			'</div>';
+		
+		return $tag;
+	}
 }
